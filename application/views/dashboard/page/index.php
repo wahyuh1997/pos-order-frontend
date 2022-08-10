dce062e99f894e708de826cbfef689780997cbd9
wahyuh1997 <wahyuh1997@gmail.com>
2022-08-10 22:00:33 +0700

    Update Dashboard Fix.

--- application/views/dashboard/page/index.php
+++ application/views/dashboard/page/index.php
@@ -9,61 +9,34 @@
 <!-- BEGIN row -->
 <div class="row">
   <!-- BEGIN col-3 -->
-  <div class="col-xl-3 col-md-6">
-    <div class="widget widget-stats bg-teal">
-      <div class="stats-icon stats-icon-lg"><i class="fa fa-globe fa-fw"></i></div>
-      <div class="stats-content">
-        <div class="stats-title">TODAY'S VISITOR</div>
-        <div class="stats-number">2,950</div>
-        <div class="stats-progress progress">
-          <div class="progress-bar" style="width: 70.1%;"></div>
-        </div>
-        <div class="stats-desc">Better than last week (70.1%)</div>
-      </div>
-    </div>
-  </div>
-  <!-- END col-3 -->
-  <!-- BEGIN col-3 -->
-  <div class="col-xl-3 col-md-6">
+  <div class="col-xl-4 col-md-6">
     <div class="widget widget-stats bg-blue">
       <div class="stats-icon stats-icon-lg"><i class="fa fa-dollar-sign fa-fw"></i></div>
       <div class="stats-content">
         <div class="stats-title">TODAY'S REVENUE</div>
         <div class="stats-number">180,200</div>
-        <div class="stats-progress progress">
-          <div class="progress-bar" style="width: 40.5%;"></div>
-        </div>
-        <div class="stats-desc">Better than last week (40.5%)</div>
       </div>
     </div>
   </div>
   <!-- END col-3 -->
   <!-- BEGIN col-3 -->
-  <div class="col-xl-3 col-md-6">
+  <div class="col-xl-4 col-md-6">
     <div class="widget widget-stats bg-indigo">
       <div class="stats-icon stats-icon-lg"><i class="fa fa-archive fa-fw"></i></div>
       <div class="stats-content">
         <div class="stats-title">TOTAL DISHES SELLED</div>
         <div class="stats-number">156</div>
-        <div class="stats-progress progress">
-          <div class="progress-bar" style="width: 76.3%;"></div>
-        </div>
-        <div class="stats-desc">Better than last week (76.3%)</div>
       </div>
     </div>
   </div>
   <!-- END col-3 -->
   <!-- BEGIN col-3 -->
-  <div class="col-xl-3 col-md-6">
+  <div class="col-xl-4 col-md-6">
     <div class="widget widget-stats bg-gray-900">
       <div class="stats-icon stats-icon-lg"><i class="fa fa-comment-alt fa-fw"></i></div>
       <div class="stats-content">
         <div class="stats-title">TABLE USED</div>
         <div class="stats-number">178</div>
-        <div class="stats-progress progress">
-          <div class="progress-bar" style="width: 54.9%;"></div>
-        </div>
-        <div class="stats-desc">Better than last week (54.9%)</div>
       </div>
     </div>
   </div>
